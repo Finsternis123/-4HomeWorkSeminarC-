a958ecf185d2f150705440d1e189c799d352dfbe
Vladislav <zomba4o4ek100@gmail.com>
2023-01-14 17:22:50 +0300

    Second task complete

---
+++ Task№2/Program.cs
@@ -0,0 +1,14 @@
+﻿//Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+Console.WriteLine ("Enter number: "); 
+int number = int.Parse (Console.ReadLine ()!);
+
+int sum = 0;
+while (number > 0)
+{
+    sum += number % 10;
+    number = number / 10;
+}
+
+Console.Write($"{sum}");
+
+
