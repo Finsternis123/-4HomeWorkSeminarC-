f3d32edad47cfb0b251bc7352496157de68cb49c
Vladislav <zomba4o4ek100@gmail.com>
2023-01-14 17:46:07 +0300

    Third task complete

---
+++ Task№3/Program.cs
@@ -0,0 +1,15 @@
+﻿//Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран. (числа берете любые)
+int[] Array = GetArray(8); 
+Console.Write($"[{String.Join(";", Array)}]"); 
+ 
+int[] GetArray (int size) 
+{ 
+    int[] Array = new int[size]; 
+    for (int i = 0; i < size; i++) 
+    { 
+        Array[i] = new Random().Next(); 
+    } 
+    return Array; 
+}
+
+
